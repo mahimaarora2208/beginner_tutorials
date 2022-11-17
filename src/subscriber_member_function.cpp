52d08c89d56f9046eb827d3c92548ec7ca0d05a4
mahimaarora2208 <marora1@umd.edu>
2022-11-17 17:18:31 -0500

    added open source lisence

--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -1,5 +1,7 @@
 /************************************************************************************
+ * Apache License 2.0
  * Copyright (c) 2022 Mahima Arora
+ * 
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
