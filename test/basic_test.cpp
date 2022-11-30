5f18f0ea97fa9efbfd852dfe397e34b472de2445
mahimaarora2208 <marora1@umd.edu>
2022-11-30 16:42:06 -0500

    split test file in main and basic test.cpp

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -1,14 +1,63 @@
-// Description: Test if a simple task plan works
+/************************************************************************************
+ * Apache License 2.0
+ * Copyright (c) 2021, Mahima Arora
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ************************************************************************************/
+/**
+ *  @file    basic_test.cpp
+ *  @author  Mahima Arora
+ *  @date    11/30/2021
+ *  @version 1.0
+ *
+ *  @brief Source file to implement a simple ROS test
+ *
+ *  @section DESCRIPTION
+ *
+ *  Source file to implement a simple ROS test.  
+ * 
+ */
 
+#include <gtest/gtest.h>
 #include <rclcpp/rclcpp.hpp>
-// #include "../src/publisher_member_function.cpp"
 #include <gtest/gtest.h>
 #include <stdlib.h>
+// #include "beginner_tutorials/srv/change_string.hpp"
+/**
+ * @brief Test case to check the existence of the message_srv service
+ * @param none
+ * @return none
+ */
 
 namespace beginner_tutorials {
-class TaskPlanningFixture : public testing::Test {
+class TestingTalker : public testing::Test {
  public:
-  TaskPlanningFixture()
+  TestingTalker()
       : node_(std::make_shared<rclcpp::Node>("basic_test"))
   {
     RCLCPP_ERROR_STREAM(node_->get_logger(), "DONE WITH CONSTRUCTOR!!");
@@ -27,20 +76,33 @@ class TaskPlanningFixture : public testing::Test {
   rclcpp::Node::SharedPtr node_;
 };
 
-TEST_F(TaskPlanningFixture, talkerTest) {
-  // MinimalPublisher pub;
-  // string output = pub.defaultMessage;
+TEST_F(TestingTalker, TrueIsTrueTest) {
   std::cout << "TEST BEGINNING!!" << std::endl;
   EXPECT_TRUE(true);
-  // ASSERT_EQ(output,"Welcome to ROS2 Publisher-Subscriber package!");
 }
-}  // namespace minimal_integration_test
-
-int main(int argc, char** argv) {
-  rclcpp::init(argc, argv);
-  ::testing::InitGoogleTest(&argc, argv);
-  int result = RUN_ALL_TESTS();
-  rclcpp::shutdown();
-  std::cout << "DONE SHUTTING DOWN ROS" << std::endl;
-  return result;
+// /**
+//  * @brief Test case to check the succesful call of the message_srv service
+//  * @param none
+//  * @return none
+//  */
+// TEST(TestingTalker, testROSService) {
+//   auto serviceCallbackPtr =
+//   rclcpp::Service<beginner_tutorials::srv::ChangeString>::SharedPtr service_;
+//         std::bind(&MinimalPublisher::changeRequestString, this,
+//                   std::placeholders::_1, std::placeholders::_2);
+//   service_ = create_service<beginner_tutorials::srv::ChangeString>(
+//         "update_request", serviceCallbackPtr);
+  
+//   const std::shared_ptr<beginner_tutorials::srv::ChangeString::Request>
+//           request,
+//       std::shared_ptr<beginner_tutorials::srv::ChangeString::Response>
+//           response) {
+//     response->output = request->input;
+//     defaultMessage =
+//         response->output;  // changes default message to what was requested
+//   }
+//   request.input = "Hello Terps!!";
+//   std::string expectedString = request.input;
+//   EXPECT_EQ(expectedString, request.input);
+// }
 }
